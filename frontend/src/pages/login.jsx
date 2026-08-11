1c64247205a6308a1e733acf5443b38237e9014c
visi006 <vismaypradeep2006@gmail.com>
2026-08-11 22:18:04 +0530

    Connect frontend to deployed backend

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -29,7 +29,7 @@ function Login() {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/auth/login",
+        "https://mini-erp-crm-backend-x5gm.onrender.com/api/auth/login",
         {
           method: "POST",
           headers: {
