ad72602b6106a518a8228e16677cafb1bb2fdc51
visi006 <vismaypradeep2006@gmail.com>
2026-08-11 20:39:31 +0530

    Configure cloud database SSL

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -6,6 +6,14 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    // Enable SSL only when deploying to cloud
+    ssl: process.env.DB_SSL === "true"
+        ? {
+            rejectUnauthorized: false
+        }
+        : undefined,
+
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
